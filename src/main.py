6abb052119c9c930c343be880a86e81fd1b2eba9
Abigail Adegbiji <adegbijiabigail@gmail.com>
2024-05-04 21:45:30 -0600

    setup pygame

--- src/main.py
+++ src/main.py
@@ -1,1 +1,38 @@
-print("hello dino!")
+import pygame
+pygame.init()
+
+fps = 60
+window_size = (600, 600)
+window = pygame.display.set_mode(window_size)
+clock = pygame.time.Clock()
+
+x = 0
+size = 30
+y = window_size[1] - size
+
+delta_time = 0
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            break
+
+    window.fill("black")
+
+    size = 30
+    rect = pygame.Rect(x, y, size, size)
+    pygame.draw.rect(window, "green", rect)
+
+    keys = pygame.key.get_pressed()
+    height = 500 * delta_time
+    if keys[pygame.K_SPACE]:
+        print("jump!")
+        y -= height
+
+    pygame.display.update()
+
+    time_spent = clock.tick(fps)
+    delta_time = time_spent / 1000
+
+pygame.quit()
