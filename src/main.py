26f9fcbbc350851170c81c2bd5b0c72c6cde7d77
Abigail Adegbiji <adegbijiabigail@gmail.com>
2024-05-05 17:59:56 -0600

    basic obstables

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 pygame.init()
 
 fps = 60
@@ -9,13 +10,37 @@ clock = pygame.time.Clock()
 
 size = 30
 bottom_left = window_size[1] - size
-x, y = 0, bottom_left
+x, y = 50, bottom_left
 jumping = False
 default_velocity = 2000
 velocity = default_velocity
 acceleration = 0.01
 jumping = False
 min_height = bottom_left - size * 4.5
+
+speed = 100
+
+# TODO: fix jumping and avoid clumping obstacles close to one another
+
+
+class Obstacle:
+    def __init__(self):
+        self.size = 10
+        offset = random.randint(50, 500)
+        self.x = window_size[0] + offset
+        self.y = window_size[1] - self.size
+
+    def update(self, speed):
+        self.x -= speed * delta_time
+        if self.x < 0:
+            offset = random.randint(50, 500)
+            self.x = window_size[0] + offset
+
+    def draw(self):
+        pygame.draw.circle(window, "red", (self.x, self.y), self.size)
+
+
+obstables = [Obstacle(), Obstacle()]
 
 delta_time = 0
 running = True
@@ -24,8 +49,6 @@ while running:
         if event.type == pygame.QUIT:
             running = False
             break
-
-    window.fill("black")
 
     keys = pygame.key.get_pressed()
     pressed_space = keys[pygame.K_SPACE]
@@ -42,8 +65,16 @@ while running:
             y = bottom_left
             jumping = False
 
+    for obstacle in obstables:
+        obstacle.update(speed)
+
+    window.fill("black")
+
     rect = pygame.Rect(x, y, size, size)
     pygame.draw.rect(window, "green", rect)
+
+    for obstacle in obstables:
+        obstacle.draw()
 
     pygame.display.update()
 
