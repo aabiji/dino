013580c9273178c48c1b6715152040b5ad6b8266
Abigail Adegbiji <adegbijiabigail@gmail.com>
2024-05-07 21:22:23 -0600

    refactor part 1

--- src/main.py
+++ src/main.py
@@ -1,118 +1,135 @@
-# TODO: the code's really messy. please refactor
-# TODO: add animations
-# TODO: be able to choose small cacti as well
-# TODO: when we jump it feels like we're hitting a ceiling. why?
+import os
+import random
+
+def init_script():
+    # Disable pygame import message
+    os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
+
+    # Assure that our required sprites are present
+    spritesheet_path = "assets/spritesheet.png"
+    if not os.path.exists(spritesheet_path):
+        print("Game assets not found")
+        exit()
+init_script()
 
 import pygame
-import random
 pygame.init()
 
-fps = 60
-window_width = 600
-window_height = 600
-window = pygame.display.set_mode((window_width, window_height))
-pygame.display.set_caption("dino")
-clock = pygame.time.Clock()
+class Spritesheet:
+    def __init__(self):
+        path = "assets/spritesheet.png"
+        self.sheet = pygame.image.load(path)
+        self.width = self.sheet.get_width()
+        self.height = self.sheet.get_height()
 
-PREVIOUS_OFFSET = 0
-
-def crop_spritesheet(x, y, width, height):
-    sheet = pygame.image.load("sprite.png")
-    crop_area = pygame.Rect(x, y, width, height)
-    sprite = pygame.Surface((width, height))
-    sprite.blit(sheet, (0, 0), crop_area)
-    return sprite
+    # Return a portion of the spritesheet as a new sprite
+    def crop(self, x, y, width, height):
+        crop_area = pygame.Rect(x, y, width, height)
+        sprite = pygame.Surface((width, height))
+        sprite.blit(self.sheet, (0, 0), crop_area)
+        return sprite
 
 class Ground:
-    def __init__(self, window_width, window_height):
+    def __init__(self, window_width, window_height, spritesheet):
+        width = spritesheet.width
+        height = 25
+
+        self.sprite = spritesheet.crop(0, spritesheet.height - height, width, height)
+        self.sprite.set_colorkey((0, 0, 0)) # Make the background transparent
+        self.rects = [
+            # At the bottom left of the window
+            pygame.Rect(0, window_height - height, width, height),
+            # Immediately after the first rect, off the screen
+            # at the bottom right
+            pygame.Rect(width - window_width, window_height - height, width, height)
+        ]
         self.window_width = window_width
-        self.rect1 = pygame.Rect(0, window_height - 25, 2404, 25)
-        self.sprite1 = crop_spritesheet(0, 105, 2404, 25)
-        self.sprite1.set_colorkey((0, 0, 0))
 
-        self.rect2 = pygame.Rect(self.rect1.width - self.window_width, window_height - 25, 2404, 25)
-        self.sprite2 = crop_spritesheet(0, 105, 2404, 25)
-        self.sprite2.set_colorkey((0, 0, 0))
-
-    # We have 2 sprites, so when one goes off the screen,
-    # the other replaces it and vice versa, allowing for smooth scrolling
-    # without any choppiness
     def update(self):
-        self.rect1.x -= 1
-        self.rect2.x -= 1
+        # We use 2 sprites moving to the left
+        # to achieve smooth scrolling. When the first
+        # sprite goes completely off the screen, the
+        # second sprite replaces it and the first sprite goes
+        # immediately after the second sprite. Vice versa.
+        for i in range(2):
+            self.rects[i].x -= 1
 
-        max_scroll = self.rect1.width - self.window_width
-        if self.rect1.x <= -max_scroll: # when the end is at 0, the other sprite beginning is also at 0, so the other sprite takes its place
-            self.rect1.x = self.rect1.width - self.window_width
-        if self.rect2.x < -max_scroll:
-            self.rect2.x = self.rect2.width - self.window_width
+            # Move to the end of the other sprite when
+            # the sprite moves completely off the screen
+            if self.rects[i].x == -self.rects[i].width:
+                self.rects[i].x = self.rects[i].width - self.window_width
 
     def draw(self, canvas):
-        canvas.blit(self.sprite1, self.rect1)
-        canvas.blit(self.sprite2, self.rect2)
+        for rect in self.rects:
+            canvas.blit(self.sprite, rect)
 
 class Player:
-    def __init__(self, ground_y):
-        self.ground = ground_y
-        self.rect = pygame.Rect(50, self.ground, 30, 30)
-        self.jump_peek = self.ground - self.rect.height * 10
+    def __init__(self, ground_y, spritesheet):
+        self.sprite = spritesheet.crop(1335, 0, 90, 95)
+        self.sprite.set_colorkey((0, 0, 0)) # Make the background transparent
+
+        self.rect = self.sprite.get_rect()
+        self.rect.y = ground_y - 95
+        self.rect.x = 50
+
         self.jumping_up = False
         self.jumping_down = False
         self.jump_extra = 0
-        self.gravity = 300
+        self.max_jump_height = ground_y - self.rect.height * 3
 
-        self.sprite = crop_spritesheet(1335, 0, 90, 95)
-        self.rect.width = self.sprite.get_width()
-        self.rect.height = self.sprite.get_height()
-        self.rect.y = self.ground - self.rect.height
-        self.sprite.set_colorkey((0, 0, 0)) # make background transparent
+        self.gravity = 300
+        self.ground = ground_y
 
     def draw(self, canvas):
         canvas.blit(self.sprite, self.rect)
 
-    def jump(self):
+    def hold_jump(self):
         if self.jumping_down:
             return
-
         self.jumping_up = True
-        # Increase our jump height based on how
-        # long we jump
+        # Increase our jump height based on how long we jump
         self.jump_extra += 1
 
+    def jump_up(self, delta_time):
+        self.rect.y -= self.gravity * delta_time
+
+        peek_height = self.max_jump_height - self.jump_extra
+        if self.rect.y <= peek_height: # Reached the jump peek
+            self.rect.y = self.max_jump_height
+            self.jumping_up = False
+            self.jumping_down = True
+            self.jump_extra = 0
+
+    def jump_down(self, delta_time):
+        self.rect.y += self.gravity * delta_time
+
+        base_height = self.ground - self.rect.height
+        if self.rect.y >= base_height: # On the ground
+            self.rect.y = base_height
+            self.jumping_down = False
+
     def update(self, delta_time):
-        # Move up or down a little bit each frame
         if self.jumping_down:
-            self.rect.y += self.gravity * delta_time
-            below_ground = self.rect.y >= self.ground - self.rect.height
-            if below_ground:
-                self.rect.y = self.ground - self.rect.height
-                self.jumping_down = False
+            self.jump_down(delta_time)
+        elif self.jumping_up:
+            self.jump_up(delta_time)
 
-        if self.jumping_up:
-            self.rect.y -= self.gravity * delta_time
-            too_high = self.rect.y < self.jump_peek - self.jump_extra
-            if too_high:
-                self.rect.y = self.jump_peek
-                self.jumping_up = False
-                self.jumping_down = True
-                self.jump_extra = 0
-
-cacti_sprites = [
-    crop_spritesheet(650, 0, 52, 100),
-    crop_spritesheet(702, 0, 50, 100),
-    crop_spritesheet(752, 0, 98, 100),
-    crop_spritesheet(850, 0, 103, 100)
-]
+PREVIOUS_OFFSET = 0 # TODO: remove this
 
 class Obstacle:
-    def __init__(self, window_width, ground_y):
-        self.width = window_width
-        self.speed = 100
-        self.sprite = None
-        self.ground = ground_y
+    def __init__(self, window_width, ground_y, possible_sprites):
+        # Default values
         self.rect = pygame.Rect(0, 0, 0, 0)
+        self.sprite = pygame.Surface((0, 0))
+
+        self.speed = 100
+        self.ground = ground_y
+        self.possible_sprites = possible_sprites
+        self.window_width = window_width
+
         self.spawn()
 
+    # TODO: reimplement this
     # Choose a random offset that avoids being
     # to close to other obstacles
     def random_offset(self):
@@ -123,27 +140,49 @@ class Obstacle:
         PREVIOUS_OFFSET = offset
         return offset
 
+    # Randomly choose a sprite and offset position
     def spawn(self):
-        self.rect.x = self.width + self.random_offset()
-        self.sprite = random.choice(cacti_sprites)
-        self.sprite.set_colorkey((0, 0, 0)) # make background transparent
+        self.sprite = random.choice(self.possible_sprites)
+        self.sprite.set_colorkey((0, 0, 0)) # make the background transparent
+
         self.rect.width = self.sprite.get_width()
         self.rect.height = self.sprite.get_height()
+        self.rect.x = self.window_width + self.random_offset()
         self.rect.y = self.ground - self.rect.height
 
     def update(self, delta_time):
         self.rect.x -= self.speed * delta_time
-        if self.rect.x < -self.sprite.get_width(): # off the screen
+
+        off_the_screen = self.rect.x < -self.rect.width
+        if off_the_screen:
             self.spawn()
 
     def draw(self, canvas):
         canvas.blit(self.sprite, self.rect)
 
-ground = Ground(window_width, window_height)
-player = Player(window_height)
+# TODO: refactor the rest
+# TODO: idea: what if the spritesheet class was responsible for
+# extracting the sprites?
+fps = 60
+window_width = 600
+window_height = 600
+window = pygame.display.set_mode((window_width, window_height))
+pygame.display.set_caption("Dino")
+clock = pygame.time.Clock()
+
+sheet = Spritesheet()
+ground = Ground(window_width, window_height, sheet)
+player = Player(window_height, sheet)
+
 obstacles = []
+cacti_sprites = [
+    sheet.crop(650, 0, 52, 100),
+    sheet.crop(702, 0, 50, 100),
+    sheet.crop(752, 0, 98, 100),
+    sheet.crop(850, 0, 103, 100)
+]
 for i in range(2):
-    obstacles.append(Obstacle(window_width,window_height))
+    obstacles.append(Obstacle(window_width,window_height, cacti_sprites))
 
 delta_time = 0
 running = True
@@ -155,7 +194,7 @@ while running:
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_SPACE]:
-        player.jump()
+        player.hold_jump()
 
     ground.update()
     player.update(delta_time)
