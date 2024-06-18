7f79db6e8179e5d1050b4771d693755f822df875
Abigail Adegbiji <adegbijiabigail@gmail.com>
2024-06-17 21:41:59 -0600

    smoother detection using live streaming

--- src/hands.py
+++ src/hands.py
@@ -5,44 +5,52 @@ from mediapipe.framework.formats import landmark_pb2
 from mediapipe.tasks.python import vision, BaseOptions
 import time
 
-model_path = "assets/hand_landmarker.task"
+class Detector:
+    def __init__(self):
+        model_path = "assets/hand_landmarker.task"
+        base_options = BaseOptions(model_asset_path=model_path)
+        options = vision.HandLandmarkerOptions(base_options=base_options,
+                                            running_mode=vision.RunningMode.LIVE_STREAM,
+                                            result_callback=self.render_hand_landmarks)
+        self.detector = vision.HandLandmarker.create_from_options(options)
+        self.annotated_image = np.zeros((640,480,3), np.uint8)
 
-base_options = BaseOptions(model_asset_path=model_path)
-options = vision.HandLandmarkerOptions(base_options=base_options, num_hands=1)
-detector = vision.HandLandmarker.create_from_options(options)
+    def render_hand_landmarks(self, detection_result, image, _):
+        annotated = np.copy(image.numpy_view())
+        hand_landmarks = detection_result.hand_landmarks
 
-def render_hand_landmarks(hand_landmarks, image):
-    annotated = np.copy(image)
+        for i in range(len(hand_landmarks)):
+            hand_landmark = hand_landmarks[i]
 
-    for i in range(len(hand_landmarks)):
-        hand_landmark = hand_landmarks[i]
+            proto_landmark = landmark_pb2.NormalizedLandmarkList()
+            points = [landmark_pb2.NormalizedLandmark(x=hl.x, y=hl.y, z=hl.z) for hl in hand_landmark]
+            proto_landmark.landmark.extend(points)
 
-        proto_landmark = landmark_pb2.NormalizedLandmarkList()
-        points = [landmark_pb2.NormalizedLandmark(x=point.x, y=point.y, z=point.z) for point in hand_landmark]
-        proto_landmark.landmark.extend(points)
+            mp.solutions.drawing_utils.draw_landmarks(
+                annotated, proto_landmark, mp.solutions.hands.HAND_CONNECTIONS,
+                mp.solutions.drawing_styles.get_default_hand_landmarks_style(),
+                mp.solutions.drawing_styles.get_default_hand_connections_style(),
+            )
 
-        mp.solutions.drawing_utils.draw_landmarks(
-            annotated, proto_landmark, mp.solutions.hands.HAND_CONNECTIONS,
-            mp.solutions.drawing_styles.get_default_hand_landmarks_style(),
-            mp.solutions.drawing_styles.get_default_hand_connections_style(),
-        )
+        self.annotated_image = annotated
 
-    return annotated
+    def run(self):
+        video = cv2.VideoCapture(0)
 
-video = cv2.VideoCapture(0)
+        while True:
+            _, frame = video.read()
 
-while True:
-    ret, frame = video.read()
+            timestamp = int(round(time.time() * 1000))
+            mp_image = mp.Image(data=frame, image_format=mp.ImageFormat.SRGB)
+            self.detector.detect_async(mp_image, timestamp)
 
-    mp_image = mp.Image(data=frame, image_format=mp.ImageFormat.SRGB)
-    detection_result = detector.detect(mp_image)
-    annotated = render_hand_landmarks(detection_result.hand_landmarks, frame)
+            cv2.imshow("Hands", self.annotated_image)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
-    cv2.imshow("Hands", annotated)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+            time.sleep(0.01)
 
-    time.sleep(0.01)
+        video.release()
+        cv2.destroyAllWindows()
 
-video.release()
-cv2.destroyAllWindows()
+Detector().run()
